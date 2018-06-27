f5e55d7b2d162e8a8d593a8fc479ad6e9e3412ec
rei.kono <rei.kono@rakuten.com>
2018-06-27 06:30:29 +0000

    modify navbar

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,4 +1,18 @@
- @if (Auth::check())
+<header>
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-left" href="/"><img src="{{ secure_asset("images/logo.png") }}" alt="Monolist"></a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                     @if (Auth::check())
                         <li>
                             <a href="#">
                                 <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
@@ -33,9 +47,6 @@
                                 <li>
                                     <a href="{{ route('logout.get') }}">ログアウト</a>
                                 </li>
-                            </ul>
-                        </li>
-                    @else                       </li>
                             </ul>
                         </li>
                     @else
